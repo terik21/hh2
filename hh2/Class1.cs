e76d17ac67228ee7e02798efced093e11b219796
terik21 <151690487+terik21@users.noreply.github.com>
2024-04-27 01:05:30 +0300

    Add files via upload

---
+++ hh2/Class1.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace hh2
+{
+    internal class Class1
+    {
+        public  static double Sqvear(double x, double y, double z)
+        {
+            double a = Math.Min(x, Math.Min(z, y));
+            double c = Math.Max(x, Math.Max(z, y));
+            double b = (x + y + z) - c - a;
+            double p = (x + y + z) / 2;
+            if (Math.Pow(a, 2) + Math.Pow(b, 2) == Math.Pow(c, 2)) return (a * b) / 2;
+            else return Math.Sqrt(p * (p - z) * (p - x) * (p - y));
+        }
+        public static double Sqvear(double x)
+        {
+            return Math.PI * Math.Pow(x, 2);
+        }
+        public static double Sqvear(double x, double y)
+        {
+            return x * y;
+        }
+    }
+}
---
+++ hh2/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+namespace hh2
+{
+    class Programm
+    {
+        static void Main()
+        {
+
+            Console.WriteLine("Памятка!\n" +
+                "Площадь какой фигуры вы хотите найти?\n" +
+                "Каждая Фигура соответствует своей цифре:\n" +
+                "1 - круг\n" +
+                "2 - квадрат/прямоугольник\n" +
+                "3 - треугольник");
+            int n = int.Parse(Console.ReadLine());
+            double a, b, c;
+            if (n == 1)
+            {
+                Console.Write("Введите радиус:");
+                a = double.Parse(Console.ReadLine());
+                Console.WriteLine($"S = {Class1.Sqvear(a)}");
+            }
+            else if (n == 2)
+            {
+                Console.Write("Введите сторону a:");
+                a = double.Parse(Console.ReadLine());
+                Console.Write("Введите сторону b:");
+                b = double.Parse(Console.ReadLine());
+                Console.WriteLine($"S = {Class1.Sqvear(a,b)}");
+            }
+            else if (n == 3)
+            {
+                Console.Write("Введите сторону a:");
+                a = double.Parse(Console.ReadLine());
+                Console.Write("Введите сторону b:");
+                b = double.Parse(Console.ReadLine());
+                Console.Write("Введите сторону c:");
+                c = double.Parse(Console.ReadLine());
+                Console.WriteLine($"S = {Class1.Sqvear(a,b,c)}");
+            }
+        }
+    }
+}
